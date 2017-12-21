f8c0c19b087505de51287717521c74aa25360765
risonakamo <hmykanae@gmail.com>
2017-12-20 21:48:07 -0500

    index h file

---
+++ index.js.h
@@ -0,0 +1,13 @@
+rowCard[] _cards;
+element _cardZone;
+
+void main();
+
+void resetCards();
+
+void-callback getCard(string card,function callback(object data));
+
+rowCard array2Card(array data);
+
+void randomiseArray(array array);
+int randint(int min,int max);
