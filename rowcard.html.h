7d35a7108fa54b436bd55c35d0c6c29b053fc024
risonakamo <hmykanae@gmail.com>
2017-12-21 17:37:52 -0500

    code formattings

--- rowcard.html.h
+++ rowcard.html.h
@@ -1,22 +1,28 @@
 class rowCard
 {
-    rowCard();
+    rowCard(string kanji,string hiragan,string colour);
     ready();
 
+    /*-- data --*/
     string kanjiText; //saved string data of card for resetting
     string hiraganaText;
     string savedColour;
+    int marked;
 
     bound-string hideColour; //colour of hide panel, html bounded
 
+    /*-- ids --*/
     element-id slide-main;
     element-id other-box;
     element-id hide-box;
     element-id top-text;
     element-id bot-text;
 
+    /*-- main action functions --*/
     void-handler unhide();
     void setMode(int mode);
+    void toggleMarked();
 
+    /*-- helpers --*/
     void-handler dragslide(event e);
 }
