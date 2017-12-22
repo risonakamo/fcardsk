77b54e7afb4a90603489c832f191bb4665dc974d
risonakamo <hmykanae@gmail.com>
2017-12-22 14:50:59 -0500

    main kwriter json processing

--- kcardwriter/kcwriter.js
+++ kcardwriter/kcwriter.js
@@ -7,15 +7,53 @@ function main()
     dropzone.addEventListener("drop",(e)=>{
         e.preventDefault();
 
-        var f=new FileReader();
+        var datafile=e.dataTransfer.items[0].getAsFile();
+        var datafileNameSplit=datafile.name.split(".");
 
+        if (datafileNameSplit[datafileNameSplit.length-1]!="xlsx")
+        {
+            console.log("not an xlsx file");
+            return;
+        }
+
+        var f=new FileReader();
         f.onload=()=>{
             var wb=XLSX.read(f.result,{type:"binary"});
 
-            console.log(wb);
+            var jsondata=XLSX.utils.sheet_to_json(wb.Sheets.Sheet1,{
+                header:1
+            });
+
+            var formatdata=[];
+            var rubys;
+            var formatrubys;
+            for (var x=0,l=jsondata.length;x<l;x++)
+            {
+                if (jsondata[x].length>3)
+                {
+                    rubys=jsondata[x].slice(3);
+                    formatrubys=[];
+                    for (var y=0,yl=rubys.length;y<yl;y+=2)
+                    {
+                        formatrubys.push([rubys[y],rubys[y+1]]);
+                    }
+
+                    formatdata.push({
+                        maindata:jsondata[x].slice(0,3),
+                        rubys:formatrubys
+                    });
+                }
+
+                else
+                {
+                    formatdata.push({maindata:jsondata[x]});
+                }
+            }
+
+            console.log(formatdata);
         };
 
-        f.readAsBinaryString(e.dataTransfer.items[0].getAsFile());
+        f.readAsBinaryString(datafile);
     });
 
     dropzone.addEventListener("dragover",(e)=>{
